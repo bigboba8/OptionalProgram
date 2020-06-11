7a2115ce122898c6d9995bef48763d3a7ae09788
ThomasOcean <ThomasOceanMartinez@gmail.com>
2020-06-10 21:49:46 -0400

    dded default Junit test f

---
+++ src/com/cognixia/jump/corejava/ems/DepartmentsTest.java
@@ -0,0 +1,14 @@
+package com.cognixia.jump.corejava.ems;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepartmentsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
